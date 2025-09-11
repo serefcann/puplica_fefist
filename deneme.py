3f009924711d6c6c5e91b2f33741530005e62559
şeref can memiş <canmemis@marun.edu.tr>
2025-09-11 16:11:12 +0300

    universite ve bolum id dosyası eklendi

---
+++ deneme.py
@@ -0,0 +1,15 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+df = pd.read_csv(r'C:\Users\şerefcanmemiş\Documents\projects_2\puplica\program_ids.csv')
+# universite ve bolum id parametreleri
+
+url = 'https://yokatlas.yok.gov.tr/lisans-univ.php?u=1072'
+response = requests.get(url, verify=False)
+soup = BeautifulSoup(response.content, 'html.parser')
+soup.prettify()
+# Örnek: Üniversite adı
+uni_name = soup.find('h3').text.strip()
+print(f"Üniversite Adı: {uni_name}")
+
+

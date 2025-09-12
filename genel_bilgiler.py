7c44a2c1c89c77f887a07bfddcebeee6de40ff9c
şeref can memiş <canmemis@marun.edu.tr>
2025-09-12 17:58:21 +0300

    genel bilgiler datası json olarak cekildi

--- genel_bilgiler.py
+++ genel_bilgiler.py
@@ -3,27 +3,29 @@ from bs4 import BeautifulSoup
 import warnings
 warnings.filterwarnings("ignore")
 import pandas as pd
+import httpx
+import asyncio
+import json
 df = pd.read_csv(r'C:\Users\şerefcanmemiş\Documents\projects_2\puplica\program_ids.csv')
 id_list  = list(df.Column1)
-id_list[2]
+
+
 # universite ve bolum id parametreleri
-for ids in id_list[:10]:
-    url = f'https://yokatlas.yok.gov.tr/content/lisans-dynamic/1000_1.php?y={ids}'
-    response = requests.get(url, verify=False)
-    soup = BeautifulSoup(response.content, 'html.parser')
-    tables = soup.find_all('table', {'class':'table table-bordered'})
-
-url = f'https://yokatlas.yok.gov.tr/content/lisans-dynamic/1000_1.php?y={100110045}'
-response = requests.get(url, verify=False)
-soup = BeautifulSoup(response.content, 'html.parser')
-
-for ids in id_list[:10]:
-    url = f'https://yokatlas.yok.gov.tr/content/lisans-dynamic/1000_1.php?y={ids}'
-    response = requests.get(url, verify=False)
-    soup = BeautifulSoup(response.content, 'html.parser')
-    tables = soup.find_all('table', {'class':'table table-bordered'})
+    
+sem = asyncio.Semaphore(5)
+async def fetch(url):
+    async with sem:
+        async with httpx.AsyncClient(verify=False) as client:
+            response = await client.get(url)
+            if response.status_code != 200:
+                raise Exception(f"Request failed with status {response.status_code}")
+        return response.text
+            
+def parse_html(html_content):
     program_info = {}
+    soup = BeautifulSoup(html_content.replace("---", "0"), 'html.parser')
     tables = soup.find_all('table', {'class':'table table-bordered'})
+    program_info["Bölüm Adı"] = tables[0].find('th').text
     for table in tables:
         rows = table.find_all('tr')
         for row in rows:
@@ -32,4 +34,19 @@ for ids in id_list[:10]:
                 key = cols[0].get_text(strip=True).replace('*','')
                 value =cols[1].get_text(strip=True).replace('*','')
                 program_info[key] = value
-    program_info
+    return program_info
+
+async def main(id_list):
+    urls = id_list[0:100]
+    tasks =[fetch(f'https://yokatlas.yok.gov.tr/content/lisans-dynamic/1000_1.php?y={url}') 
+            for url in urls]
+    results = await asyncio.gather(*tasks)  
+    parsed_data = [parse_html(html) for html in results]
+    return parsed_data
+
+parsed_data = asyncio.run(main(id_list))
+
+with open ("yokatlas_data.json", 'w', encoding='utf-8') as f:
+    json.dump(parsed_data, f, ensure_ascii=False, indent=3)
+
+
